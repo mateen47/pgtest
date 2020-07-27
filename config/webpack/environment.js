e60e4d45962923a76b3d3d89d9f4dc25930deff2
mateen47 <mateen.ismail17@gmail.com>
2020-07-27 13:53:38 +0500

    add to heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,4 @@
 const { environment } = require('@rails/webpacker')
 
-var webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-	$: 'jquery',
-	jquery: 'jquery',
-	Propper: ['propper.js', 'default']
-}))
+
 module.exports = environment
